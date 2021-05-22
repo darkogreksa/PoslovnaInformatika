f264584204225f67c7d77181421b8b5087f6dd77
Marko Topic <markotopic97@hotmail.com>
2021-05-22 13:33:54 +0200

    Spring initializr project

---
+++ src/test/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.poslovna.informatika.warehouse.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
