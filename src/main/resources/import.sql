cc30ae2c2297c4e8b899117121b00c7a2130f7bc
greksadarko <greksadarko@gmail.com>
2021-06-13 18:03:40 +0200

    It works!

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,6 @@ INSERT INTO location(name) VALUES('Novi Sad');
 
 INSERT INTO company(name, pib, address, location_id) values ('Continental', '85479658', 'Radnicka 28', 1);
 
-INSERT INTO groupOfProducts(name, company_id) VALUES('Hrana', 1);
+INSERT INTO group_of_products(name, company_id) VALUES('Hrana', 1);
 
-INSERT INTO product(name, groupOfProducts_id, unit_id) VALUES ('Brasno', 1, 1);
+INSERT INTO product(name, group_of_products_id, unit_id) VALUES ('Brasno', 1, 1);
