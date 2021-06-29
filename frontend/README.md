569dbdb673e3fd4625346e4cc59cf67d2e207acd
greksadarko <greksadarko@gmail.com>
2021-06-29 20:18:56 +0200

    List of units

--- frontend/README.md
+++ frontend/README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Angular command cheat sheet
+`ng serve --proxy-config proxy.config.json`
+`ng generate service xxx`
+`ng generate component xxx`
+`ng generate class xxx`
---
+++ src/main/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/controller/UnitController.java
@@ -0,0 +1,71 @@
+package rs.ac.uns.ftn.poslovna.informatika.warehouse.business.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.dto.UnitDTO;
+import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.model.Unit;
+import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.service.implementation.UnitService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(value = "/api/unit")
+public class UnitController {
+
+    @Autowired
+    UnitService unitService;
+
+    @GetMapping
+    public ResponseEntity<List<UnitDTO>> getAll(){
+        List<Unit> units = unitService.findAll();
+        List<UnitDTO> unitDTOS = new ArrayList<>();
+        for(Unit unit : units) {
+            unitDTOS.add(new UnitDTO(unit));
+        }
+        return new ResponseEntity<List<UnitDTO>>(unitDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UnitDTO> getOne(@PathVariable("id") Integer id){
+        Unit unit = unitService.findOne(id);
+        if(unit == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<UnitDTO>(new UnitDTO(unit), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/create", consumes = "application/json")
+    public ResponseEntity<UnitDTO> create(@RequestBody UnitDTO unitDTO){
+        Unit unit = new Unit();
+        unit.setName(unitDTO.getName());
+
+        unit = unitService.save(unit);
+        return new ResponseEntity<UnitDTO>(new UnitDTO(), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping(value = "/update/{id}", consumes = "application/json")
+    public ResponseEntity<UnitDTO> update(@PathVariable("id") Integer id, @RequestBody UnitDTO unitDTO){
+        Unit unit = unitService.findOne(id);
+        if(unit == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        unit.setName(unitDTO.getName());
+        unit = unitService.save(unit);
+        return new ResponseEntity<UnitDTO>(new UnitDTO(unit), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+        Unit unit = unitService.findOne(id);
+        if(unit == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        unitService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
