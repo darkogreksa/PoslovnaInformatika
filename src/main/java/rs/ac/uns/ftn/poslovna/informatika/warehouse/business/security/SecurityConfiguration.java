e8a72d9550bbc9c1d27abac2e864e04dab3c1858
Marko Topic <markotopic97@hotmail.com>
2021-06-29 23:06:21 +0200

    Added CORS config in Spring

--- src/main/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/security/SecurityConfiguration.java
+++ src/main/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/security/SecurityConfiguration.java
@@ -52,6 +52,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .cors();
+        httpSecurity
                 .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
