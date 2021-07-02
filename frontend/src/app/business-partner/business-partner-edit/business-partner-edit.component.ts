276d2a40636f27d09d39c46e2a04f54bed946e29
greksadarko <greksadarko@gmail.com>
2021-07-02 19:50:19 +0200

    Edit business partner

--- frontend/src/app/business-partner/business-partner-edit/business-partner-edit.component.ts
+++ frontend/src/app/business-partner/business-partner-edit/business-partner-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { BusinessPartner } from 'src/app/model/business-partner';
+import { BusinessPartnerService } from 'src/app/services/business-partner.service';
 
 @Component({
   selector: 'app-business-partner-edit',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinessPartnerEditComponent implements OnInit {
 
-  constructor() { }
+  partner: BusinessPartner;
+  editBP:  FormGroup;
+  formBuilder : FormBuilder;
+  sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(private partnerService: BusinessPartnerService, private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.editBP = new FormGroup({
+      inputName: new FormControl(),
+      inputPib: new FormControl(),
+      inputAddress: new FormControl(),
+    });
+    
+    this.sub = this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.partnerService.getId(id).subscribe((p: BusinessPartner) => {
+          if (p) {
+            console.log(p);
+            this.partner = p;
+          } else {
+              alert("Nepostojeci partner");
+          }
+        });
+      }
+    });
   }
 
+  edit(partner: BusinessPartner, id){
+    console.log(id);
+    const name: string = this.editBP.controls.inputName.value;
+    const pib: any = this.editBP.controls.inputPib.value;
+    const address: any = this.editBP.controls.inputAddress.value;
+
+    this.partner.name = name;
+    this.partner.pib = pib;
+    this.partner.address = address;
+
+    console.log(partner);
+
+    this.partnerService.edit(partner, id).subscribe(partner => this.partner);
+    window.location.replace("business-partner");
+  }
 }
