81686c87b08111f8b5eb9c2c7276ac057d858e95
greksadarko <greksadarko@gmail.com>
2021-08-01 19:49:20 +0200

    Product card backend update

--- src/main/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/controller/ProductCardController.java
+++ src/main/java/rs/ac/uns/ftn/poslovna/informatika/warehouse/business/controller/ProductCardController.java
@@ -1,6 +1,7 @@
 package rs.ac.uns.ftn.poslovna.informatika.warehouse.business.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,8 @@ import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.dto.ProductDTO;
 import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.model.Product;
 import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.model.ProductCard;
 import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.model.Warehouse;
+import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.service.BusinessYearServiceInterface;
+import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.service.ProductServiceInterface;
 import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.service.implementation.ProductCardService;
 import rs.ac.uns.ftn.poslovna.informatika.warehouse.business.service.implementation.WarehouseService;
 
@@ -25,6 +28,12 @@ public class ProductCardController {
 
     @Autowired
     ProductCardService productCardService;
+
+    @Autowired
+    BusinessYearServiceInterface businessYearServiceInterface;
+
+    @Autowired
+    ProductServiceInterface productServiceInterface;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductCardDTO> getById(@PathVariable("id") Integer id) {
@@ -46,6 +55,91 @@ public class ProductCardController {
             productCardDTOS.add(new ProductCardDTO(productCard));
         }
         return new ResponseEntity<List<ProductCardDTO>>(productCardDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity<List<ProductCardDTO>> getAll(){
+        List<ProductCard> productCards = productCardService.findAll();
+        List<ProductCardDTO> productCardDTOS = new ArrayList<>();
+        for (ProductCard productCard : productCards){
+            productCardDTOS.add(new ProductCardDTO(productCard));
+        }
+        if (productCardDTOS == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(productCardDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"", "/"}, params = {"page", "size"})
+    public ResponseEntity<Page<ProductCardDTO>> getAllPaged(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        Page<ProductCard> productCards = (Page<ProductCard>) productCardService.findAllPaged(page, size);
+        Page<ProductCardDTO> productCardDTOS = productCards.map(ProductCardDTO::new);
+
+        return new ResponseEntity<Page<ProductCardDTO>>(productCardDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/business-year")
+    public ResponseEntity<List<ProductCardDTO>> getAllByOrderByBusinessYearDesc(){
+        List<ProductCard> productCards = productCardService.findAllByOrderByBusinessYearDesc();
+        List<ProductCardDTO> productCardDTOS = new ArrayList<ProductCardDTO>();
+        for (ProductCard productCard : productCards) {
+            productCardDTOS.add(new ProductCardDTO(productCard));
+        }
+        return new ResponseEntity<List<ProductCardDTO>>(productCardDTOS, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/add", consumes = "application/json")
+    public ResponseEntity<ProductCardDTO> addProductCard(@RequestBody ProductCardDTO productCardDTO){
+        ProductCard productCard = new ProductCard();
+        productCard.setPrice(productCardDTO.getPrice());
+        productCard.setInitialStateQuantity(productCardDTO.getInitialStateQuantity());
+        productCard.setInitialStateValue(productCardDTO.getInitialStateValue());
+        productCard.setInboundTrafficQuantity(productCardDTO.getInboundTrafficQuantity());
+        productCard.setInboundTrafficValue(productCardDTO.getInboundTrafficValue());
+        productCard.setOutgoingTrafficQuantity(productCardDTO.getOutgoingTrafficQuantity());
+        productCard.setOutgoingTrafficValue(productCardDTO.getOutgoingTrafficValue());
+        productCard.setTotalAmount(productCardDTO.getTotalAmount());
+        productCard.setTotalValue(productCardDTO.getTotalValue());
+        productCard.setBusinessYear(businessYearServiceInterface.findOne(productCardDTO.getBusinessYear().getId()));
+        productCard.setProduct(productServiceInterface.findOne(productCardDTO.getProduct().getId()));
+        productCard.setWarehouse(warehouseService.findOne(productCardDTO.getWarehouse().getId()));
+
+        productCard = productCardService.save(productCard);
+
+        return new ResponseEntity<ProductCardDTO>(new ProductCardDTO(productCard), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/update/{id}", consumes = "application/json")
+    public ResponseEntity<ProductCardDTO> updateProductCard(@RequestBody ProductCardDTO productCardDTO, @PathVariable("id") Integer id){
+        ProductCard productCard = productCardService.findOne(id);
+        if(productCard == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        productCard.setId(productCardDTO.getId());
+        productCard.setPrice(productCardDTO.getPrice());
+        productCard.setInitialStateQuantity(productCardDTO.getInitialStateQuantity());
+        productCard.setInitialStateValue(productCardDTO.getInitialStateValue());
+        productCard.setInboundTrafficQuantity(productCardDTO.getInboundTrafficQuantity());
+        productCard.setInboundTrafficValue(productCardDTO.getInboundTrafficValue());
+        productCard.setOutgoingTrafficQuantity(productCardDTO.getOutgoingTrafficQuantity());
+        productCard.setOutgoingTrafficValue(productCardDTO.getOutgoingTrafficValue());
+        productCard.setTotalAmount(productCardDTO.getTotalAmount());
+        productCard.setTotalValue(productCardDTO.getTotalValue());
+        productCard.setBusinessYear(businessYearServiceInterface.findOne(productCardDTO.getBusinessYear().getId()));
+
+        productCard = productCardService.save(productCard);
+
+        return new ResponseEntity<ProductCardDTO>(new ProductCardDTO(productCard), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+        ProductCard productCard = productCardService.findOne(id);
+        if(productCard == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        productCardService.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
 }
